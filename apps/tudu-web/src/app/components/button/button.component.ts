eaf40254dda49690adcf99794d7822dfc05c843d
Krisztian Kugler <krisztian.kugler@gmail.com>
2024-04-11 19:13:21 +0200

    add customizable spinner component

--- apps/tudu-web/src/app/components/button/button.component.ts
+++ apps/tudu-web/src/app/components/button/button.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, HostBinding, booleanAttribute, input } from "@angular/core";
 
 import { IconComponent } from "../icon/icon.component";
-import { IconName } from "src/provide-icons";
+import { SpinnerComponent } from "../spinner/spinner.component";
+
+import type { IconName } from "src/provide-icons";
 
 type ButtonVariant = "primary" | "secondary" | "tertiary";
 
 @Component({
   selector: "button [tudu-button]",
   standalone: true,
-  imports: [IconComponent],
+  imports: [IconComponent, SpinnerComponent],
   templateUrl: "./button.component.html",
   styleUrl: "./button.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,14 +19,14 @@ export class ButtonComponent {
   variant = input<ButtonVariant>("primary");
   leadingIcon = input<IconName>();
   trailingIcon = input<IconName>();
-  disabled = input<boolean>(false);
-  loading = input<boolean>(false);
+  disabled = input<boolean, unknown>(false, { transform: booleanAttribute });
+  loading = input<boolean, unknown>(false, { transform: booleanAttribute });
 
-  @HostBinding("disabled") get disabledState() {
+  @HostBinding("disabled") get disabledState(): boolean {
     return this.disabled();
   }
 
-  @HostBinding("class") get variantClass() {
+  @HostBinding("class") get variantClass(): ButtonVariant {
     return this.variant();
   }
 }
---
+++ apps/tudu-web/src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input } from "@angular/core";
+
+@Component({
+  selector: "tudu-spinner",
+  standalone: true,
+  template: "",
+  styleUrl: "./spinner.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SpinnerComponent {
+  size = input<number>(24);
+  thickness = input<number>(4);
+  color = input<string>("yellow");
+  duration = input<number>(500);
+
+  @HostBinding("style.--size") get sizeValue(): string {
+    return this.size() + "px";
+  }
+
+  @HostBinding("style.--thickness") get thicknessValue(): string {
+    return this.thickness() + "px";
+  }
+
+  @HostBinding("style.--color") get colorValue(): string {
+    return this.color();
+  }
+
+  @HostBinding("style.--duration") get durationValue(): string {
+    return this.duration() + "ms";
+  }
+}
