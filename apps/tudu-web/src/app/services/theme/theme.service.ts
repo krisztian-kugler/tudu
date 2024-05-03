cb3762dcd0d289c3c00e28729c7e1936c47703fe
Krisztian Kugler <krisztian.kugler@gmail.com>
2024-05-03 17:47:15 +0200

    refactor theme service

--- apps/tudu-web/src/app/services/theme/theme.service.ts
+++ apps/tudu-web/src/app/services/theme/theme.service.ts
@@ -1,42 +1,33 @@
 import { DOCUMENT } from "@angular/common";
 import { Inject, Injectable, Renderer2 } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, tap } from "rxjs";
 
 import { BrowserStorageService, StorageKey } from "../browser-storage/browser-storage.service";
 
 export type Theme = "light" | "dark";
 
-const defaultTheme: Theme = "light";
-
 @Injectable({
   providedIn: "root",
 })
 export class ThemeService {
-  public themeChange$ = new BehaviorSubject<Theme>(this.theme);
+  theme$ = new BehaviorSubject<Theme>(this.browserStorageService.get<Theme>(StorageKey.Theme) || "light");
 
-  private _theme: Theme = defaultTheme;
-
-  private get theme(): Theme {
-    return this._theme;
-  }
-
-  private set theme(value: Theme) {
-    this._theme = value;
-    this.renderer.setAttribute(this.document.documentElement, "data-theme", value);
-    this.themeChange$.next(value);
-    this.browserStorageService.set(StorageKey.Theme, value);
-  }
+  private applyTheme$ = this.theme$.pipe(
+    tap((value) => {
+      this.renderer.setAttribute(this.document.documentElement, "data-theme", value);
+      this.browserStorageService.set(StorageKey.Theme, value);
+    })
+  );
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2,
     private browserStorageService: BrowserStorageService
   ) {
-    const storedTheme = this.browserStorageService.get<Theme>(StorageKey.Theme);
-    this.theme = storedTheme || defaultTheme;
+    this.applyTheme$.subscribe();
   }
 
-  toggleTheme() {
-    this.theme = this.theme === "light" ? "dark" : "light";
+  toggle() {
+    this.theme$.next(this.theme$.value === "light" ? "dark" : "light");
   }
 }
