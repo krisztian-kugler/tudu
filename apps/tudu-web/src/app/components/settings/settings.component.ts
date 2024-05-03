074409ab89babf1762da81fdb613227dc38e467c
Krisztian Kugler <krisztian.kugler@gmail.com>
2024-05-03 17:51:09 +0200

    create settings page component

---
+++ apps/tudu-web/src/app/components/settings/settings.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: "tudu-settings",
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: "./settings.component.html",
+  styleUrl: "./settings.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SettingsComponent {
+  navItems = [
+    {
+      label: "General",
+      path: "general",
+    },
+    {
+      label: "Notifications",
+      path: "notifications",
+    },
+    {
+      label: "Appearance",
+      path: "appearance",
+    },
+    {
+      label: "Chat",
+      path: "chat",
+    },
+    {
+      label: "Account",
+      path: "account",
+    },
+  ];
+}
