1cbc60aba2b0021a55b30b93072b85af8adfb52a
Krisztian Kugler <krisztian.kugler@gmail.com>
2024-09-28 11:10:03 +0200

    remove almost all test files (will re-create them later)

--- apps/tudu-web/src/app/shared/features/drag-drop/drag-area.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragAreaDirective } from './drag-area.directive';
-
-describe('DragAreaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DragAreaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
